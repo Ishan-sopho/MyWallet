e013c2537d1a9ce38910db352eaa4d6fbeaf5a3e
Nishchay-sopho <agrawal.nishchay5@gmail.com>
2017-09-01 23:22:04 +0530

    Added export to CSV feature

--- app/src/main/java/twitu/mywallet/wallet.java
+++ app/src/main/java/twitu/mywallet/wallet.java
@@ -1,41 +1,85 @@
 package twitu.mywallet;
 
+import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Build;
+import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.support.annotation.IntegerRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.opencsv.CSVWriter;
+
 import org.w3c.dom.Text;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
 public class wallet extends AppCompatActivity {
+
     Dbhelper dbhelper = new Dbhelper(this);
+    private Toolbar toolbar;
+    private SharedPreferences flag;
+    public static final int REQUEST_EXTERNAL_PERMISSION_CODE = 666;
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    public static final String[] PERMISSIONS_EXTERNAL_STORAGE = {
+            READ_EXTERNAL_STORAGE,
+            WRITE_EXTERNAL_STORAGE
+    };
+
+    private SQLiteDatabase walletDb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wallet);
 
-        final SQLiteDatabase walletDb = dbhelper.getWritableDatabase();
+        toolbar= (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("MyWallet");
+        setSupportActionBar(toolbar);
+
+        walletDb = dbhelper.getWritableDatabase();
         final TextView display = (TextView) findViewById(R.id.cash);
         Button pay = (Button) findViewById(R.id.pay);
         Button receive = (Button) findViewById(R.id.receive);
         final EditText info = (EditText) findViewById(R.id.info);
         final EditText amount = (EditText) findViewById(R.id.amount);
 
-        SharedPreferences flag = getSharedPreferences("flag", Context.MODE_PRIVATE);
+         flag= getSharedPreferences("flag", Context.MODE_PRIVATE);
 //        SharedPreferences flag= PreferenceManager.getDefaultSharedPreferences(this);
         final SharedPreferences.Editor flagEditor = flag.edit();
 
@@ -94,5 +138,105 @@ public class wallet extends AppCompatActivity {
         display.setText(currentBalance);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
 
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.export :
+                checkExternalStoragePermission(this);
+                if(checkExternalStoragePermission(this))
+                    exportToCSV();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public boolean checkExternalStoragePermission(Activity activity) {
+        if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            return true;
+        }
+
+        int readStoragePermissionState = ContextCompat.checkSelfPermission(activity, READ_EXTERNAL_STORAGE);
+        int writeStoragePermissionState = ContextCompat.checkSelfPermission(activity, WRITE_EXTERNAL_STORAGE);
+        boolean externalStoragePermissionGranted = readStoragePermissionState == PackageManager.PERMISSION_GRANTED &&
+                writeStoragePermissionState == PackageManager.PERMISSION_GRANTED;
+        if (!externalStoragePermissionGranted) {
+            requestPermissions(PERMISSIONS_EXTERNAL_STORAGE, REQUEST_EXTERNAL_PERMISSION_CODE);
+        }
+
+        return externalStoragePermissionGranted;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (requestCode == REQUEST_EXTERNAL_PERMISSION_CODE) {
+                if(checkExternalStoragePermission(this))
+                    exportToCSV();
+            }
+        }
+    }
+
+    private void exportToCSV(){
+        Log.d("TAG","Inside export to CSV");
+        SQLiteDatabase db=dbhelper.getReadableDatabase();
+        Cursor cursor=db.rawQuery("SELECT * from moneyTransaction",null);
+        String rowID=flag.getString("lastRow",null);
+        int numberOfRows=Integer.valueOf(rowID);
+        String path = Environment.getExternalStorageDirectory()+"/Download/"+"wallet.csv";
+        CSVWriter writer=null;
+        try{
+            writer=new CSVWriter(new FileWriter(path));
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        List<String[]> data=new ArrayList<String[]>();
+        data.add(new String[]{"TimeStamp","Transaction type","Amount","Current Balance","Description"});
+        while(cursor.moveToNext()){
+            String timeStamp=cursor.getString(cursor.getColumnIndexOrThrow(transaction.COLUMN_TRANSACTION_TIME));
+            String transactionType=cursor.getString(cursor.getColumnIndexOrThrow(transaction.COLUMN_TRANSACTION_TYPE));
+            String amount=cursor.getString(cursor.getColumnIndexOrThrow(transaction.COLUMN_TRANSACTION_AMOUNT));
+            String balance=cursor.getString(cursor.getColumnIndexOrThrow(transaction.COLUMN_TRANSACTION_BALANCE));
+            String description=cursor.getString(cursor.getColumnIndexOrThrow(transaction.COLUMN_TRANSACTION_DESCRIPTION));
+//            Log.d("TAG","Transaction detected !");
+                data.add(new String[]{convertTimeStampToDate(timeStamp),transactionType,amount,balance,description});
+        }
+        if (writer != null) {
+            writer.writeAll(data);
+            try {
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Toast.makeText(this, "Successfully made CSV !!", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Some error occured!", Toast.LENGTH_SHORT).show();
+        }
+        if(cursor.isLast()){
+            cursor.close();
+        }
+
+        db.close();
+    }
+
+    private String convertTimeStampToDate(String stamp){
+        long timeStamp=Long.parseLong(stamp);
+        DateFormat objFormatter = new SimpleDateFormat("dd MMM yyyy hh:mm aa",Locale.getDefault());
+        objFormatter.setTimeZone(TimeZone.getDefault());
+
+        Calendar objCalendar =
+                Calendar.getInstance(TimeZone.getDefault());
+        objCalendar.setTimeInMillis(timeStamp * 1000);//edit
+        String result = objFormatter.format(objCalendar.getTime());
+        objCalendar.clear();
+        return result;
+    }
 }
