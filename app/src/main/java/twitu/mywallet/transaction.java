187e418d5ec38181e4f910169b0b1cd4cf447829
Nishchay-sopho <agrawal.nishchay5@gmail.com>
2017-12-13 16:25:21 +0530

    started implementing multiple wallets

--- app/src/main/java/twitu/mywallet/transaction.java
+++ app/src/main/java/twitu/mywallet/transaction.java
@@ -8,14 +8,26 @@ import java.security.PublicKey;
  * Created by Ishan on 24-08-2017.
  */
 
-public final class transaction implements BaseColumns{
-    //TABLE NAME
-    public static final String TABLE_NAME = "moneyTransaction";
+public final class transaction {
 
-    //TABLE COLUMNS
-    public static final String COLUMN_TRANSACTION_TYPE = "type";
-    public static final String COLUMN_TRANSACTION_AMOUNT = "amount";
-    public static final String COLUMN_TRANSACTION_BALANCE = "balance";
-    public static final String COLUMN_TRANSACTION_TIME = "time";
-    public static final String COLUMN_TRANSACTION_DESCRIPTION = "description";
+    public final class walletTransaction implements BaseColumns {
+        //TABLE NAME
+        public static final String TABLE_NAME = "moneyTransaction";
+
+        //TABLE COLUMNS
+        public static final String COLUMN_TRANSACTION_TYPE = "type";
+        public static final String COLUMN_TRANSACTION_AMOUNT = "amount";
+        public static final String COLUMN_TRANSACTION_BALANCE = "balance";
+        public static final String COLUMN_TRANSACTION_TIME = "time";
+        public static final String COLUMN_TRANSACTION_DESCRIPTION = "description";
+    }
+
+    public final class wallets implements BaseColumns {
+        //TABLE NAME
+        public static final String TABLE_NAME = "wallets";
+
+        //TABLE COLUMNS
+        public static final String COLUMN_WALLET_NAME = "type";
+        public static final String COLUMN_WALLET_PARENT_NAME = "amount";    
+    }
 }
